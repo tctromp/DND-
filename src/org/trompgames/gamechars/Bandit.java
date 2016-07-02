250298f65921b79544f3d0cab720733bb68796d4
Thomas <thomas_online@tromp.org>
2016-07-01 19:16:35 -0700

    d

--- src/org/trompgames/gamechars/Bandit.java
+++ src/org/trompgames/gamechars/Bandit.java
@@ -2,12 +2,15 @@ package org.trompgames.gamechars;
 
 import org.bukkit.entity.Entity;
 import org.trompgames.character.GameCharacter;
+import org.trompgames.character.MeleeAttack;
+import org.trompgames.character.RangedAttack;
 
 public class Bandit extends GameCharacter{
 
 	public Bandit(Entity entity){
 		super(entity, "Bandit", "2d8+2", 11, 9);
-		this.addMeleeAttack(new MeleeAttack());
+		this.addMeleeAttack(new MeleeAttack("Scimitar", 3, 1, "1d6+1"));
+		this.addRangedAttack(new RangedAttack("Light Crossbow", 3, 24, 96, "1d8+1"));
 	}
 	
 }
